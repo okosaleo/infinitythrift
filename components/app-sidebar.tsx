a365390781a342d5387711cab6db5d42a6d8577c
okosaleo <okosaleonel@gmail.com>
2025-01-10 18:13:02 +0100

    todays updates

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {  Banknote, HandCoins,  LayoutDashboard, PiggyBank, } from "lucide-react"
 
 import {
@@ -12,27 +13,33 @@ import {
 } from "@/components/ui/sidebar"
 import Image from "next/image"
 import Link from "next/link"
+import { Collapsible, CollapsibleTrigger } from "./ui/collapsible"
+import { CollapsibleContent } from "@radix-ui/react-collapsible"
+import { useState } from "react";
 
 // Menu items.
 const items = [
   {
-    title: "Dashboard",
-    url: "dashboard",
-    icon: LayoutDashboard,
+    title: "Thrift Savings",
+    url: "/dashboard",
   },
   {
-    title: "Saving",
-    url: "#",
-    icon: HandCoins,
+    title: "Categories / Cycles",
+    url: "/dashboard",
   },
   {
-    title: "Loan",
+    title: "Structured Savings",
     url: "#",
-    icon: Banknote,
   },
 ]
 
 export function AppSidebar() {
+  const [isvisible, setIsVisible] = useState(false)
+
+const handleClick = () => {
+  setIsVisible(true)
+}
+
   return (
     <Sidebar className="border-outline-day" >
         <SidebarHeader className="flex justify-center items-center border-b-[1px] mb-5 border-b-outline-day">
@@ -44,16 +51,48 @@ export function AppSidebar() {
         <SidebarGroup>
             <SidebarGroupContent>
                 <SidebarMenu className="flex gap-2">
-                    {items.map((item) => (
-                        <SidebarMenuItem key={item.title}>
+                        <SidebarMenuItem>
                             <SidebarMenuButton asChild>
-                                <Link href={item.url} className="flex items-center gap-4">
-                                <item.icon  className="size-4 text-content2-day"/>
-                                <span className="text-base font-medium text-content2-day">{item.title}</span>
+                                <Link href="/dashboard" className="flex items-center gap-4">
+                                <LayoutDashboard  className="size-4 text-content2-day"/>
+                                <span className="text-base font-medium text-content2-day">Dashboard</span>
                                 </Link>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
-                    ))}
+                        <Collapsible defaultOpen className="group/collapsible">
+                        <SidebarMenuItem>
+                        <CollapsibleTrigger asChild>
+                            <SidebarMenuButton asChild>
+                                <Link href="/dashboard" className="flex items-center gap-4">
+                                <HandCoins  className="size-4 text-content2-day"/>
+                                <span className="text-base font-medium text-content2-day">Savings</span>
+                                </Link>
+                            </SidebarMenuButton>
+                            </CollapsibleTrigger>
+                            <CollapsibleContent>
+                            <SidebarMenuButton asChild className="w-full flex justify-between items-center">
+                                <Link href="/dashboard" className="flex items-center gap-4">
+                                <span className="text-base font-medium text-content2-day">Thrift Savings</span>
+                                </Link>
+                            </SidebarMenuButton>
+                            </CollapsibleContent>
+                            <CollapsibleContent>
+                            <SidebarMenuButton asChild>
+                                <Link href="/dashboard" className="flex items-center gap-4">
+                                <span className="text-base font-medium text-content2-day">Categories / Cycles</span>
+                                </Link>
+                            </SidebarMenuButton>
+                            </CollapsibleContent>
+                            <CollapsibleContent>
+                            <SidebarMenuButton asChild>
+                                <Link href="/dashboard" className="flex items-center gap-4">
+                                <span className="text-base font-medium text-content2-day">Structured Savings</span>
+                                </Link>
+                            </SidebarMenuButton>
+                            <hr className="w-full text-outline-day" />
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                        </Collapsible>
                 </SidebarMenu>
             </SidebarGroupContent>
         </SidebarGroup>
